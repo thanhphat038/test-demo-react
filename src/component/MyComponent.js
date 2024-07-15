5c4290e0c74e3cc4dbd33fdfca88d950fd021701
thanhphat0308 <nguyenphat38po@gmail.com>
2024-07-15 19:23:24 +0700

    video 17

--- src/component/MyComponent.js
+++ src/component/MyComponent.js
@@ -1,9 +1,20 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+  
+  state = {
+    name: 'Phat',
+    address:'hoi dan it',
+    age: 21
+  };
+  
   // jsx
   render() {
-    return <div>my first component</div>;
+    return(
+    <div>
+      my name is {this.state.name} and im from {this.state.address}
+    </div>
+  );
   }
 }
 
